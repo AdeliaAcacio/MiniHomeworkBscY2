c6af306c1afe7b3ae909351d3828d293f17ac231
Adelia Acacio <2022203@student.cct.ie>
2023-09-30 19:08:29 +0100

    Refractor used, uppercase, output modified
    I used Refractor to change the class name to ABC program.
    I changed the outputs Welcome and Hello user.
    Add int to user answers

---
+++ src/minihomeworkbscy2/ABCProgram.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minihomeworkbscy2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Adelia Acacio 2022203
+ */
+public class ABCProgram {
+
+    /**
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+       System.out.println("          ****** WELCOME TO THE AWESOME BOOK CLUB PROGRAM!!! ******");//Output to screen
+       System.out.println("          (This program will check if you are awesome enough to join our club)\n " );
+       
+        String userName; //To store user name
+        int userAnswer1, userAnswer2;
+        
+        Scanner myKB = new Scanner(System.in); //Prompt
+        
+        System.out.println("What is your name?"); //Output to screen
+        userName = myKB.nextLine(); //Read from the screen and store in userName
+        
+        //Converting user name characters to upper case using .toUpperCase()
+        System.out.println("\n               *** HELLO, " + userName +  "***\n Let's see how awesome you really are!"); //Output to screen
+        System.out.println("\n How many books have you read last year? Numbers only");//possible error  - catch the error
+        userAnswer1 = myKB.nextInt();
+        
+        System.out.println("How many books have you read this year?");
+        userAnswer2 = myKB.nextInt();
+        
+    }
+    
+}
